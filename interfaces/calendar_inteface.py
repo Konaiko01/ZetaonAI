0a050cd33960b822f210ac2aacfc91cbac8e2e16
JoaoPedroCanavez <jsantossilvano@gmail.com>
2025-11-01 18:20:44 -0300

    Fix: adicionado import (ABC) e comentarios para refencia futura

--- interfaces/calendar_inteface.py
+++ interfaces/calendar_inteface.py
@@ -1,20 +1,31 @@
+from abc import ABC, abstractmethod
+
+'''
+Interface com o objetivo de adapatar e abstrair a aplicacao
+para que seja possivel usar mais de um cliente(API) de calendario
+'''
+
 class ICalendar(ABC):
+    #retorna o id do evento criado
+    @abstractmethod
+    def create_event(self)-> str: ...
     
+    #retorna o json com todods os eventos
     @abstractmethod
-    def create_event(self, event: dict):
-        pass
+    def get_events(self)-> str: ...
 
+    #retorna o json com evento do id passado no parametro
     @abstractmethod
-    def get_events(self):
-        pass
+    def get_event_by_id(self, id: str)-> str: ...
 
+    #retorna uma boolean que fala se a auteração foi feita
     @abstractmethod
-    def update_event(self):
-        pass
+    def update_event(self)-> bool: ...
 
+    #retorna um boolean dizendo se a delete foi bem sucedido
     @abstractmethod
-    def delete_events(self):
-        pass
+    def delete_events(self)-> bool: ...
+
 
 
     
