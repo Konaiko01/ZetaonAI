bb12556643f4d67f75cbf1f3c8446f69b8bd2233
JoaoPedroCanavez <jsantossilvano@gmail.com>
2025-11-03 04:42:02 -0300

    (Fix) Especificado o tipo de logica que deve ser usada

--- controllers/process_incoming_message.py
+++ controllers/process_incoming_message.py
@@ -1,12 +1,15 @@
 from ..utils.logger import logger
-from..services.media_processor import MediaProcessor
+from ..services.media_processor_service import MediaProcessorService
+from ..services.message_queue_service import MessageQueueService
+
 #--------------------------------------------------------------------------------------------------------------------#
 class MessageProcessController:
 #--------------------------------------------------------------------------------------------------------------------#
 
 
     def __init__(self):
-        processor = MediaProcessor()
+        processor = MediaProcessorService()
+        queue = MessageQueueService()
 
 
 #--------------------------------------------------------------------------------------------------------------------#
@@ -15,4 +18,5 @@ class MessageProcessController:
     def ProcessIncomingMessage(self, data: dict): ...
         
     #fazer logica para que receba a mensagem e coloque ela na fila(queue)
+    #usar media processor para coletar os dados e message queue para adicionar na fila
         
