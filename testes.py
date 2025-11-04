f89142fee6412ba02976f520faf175181f71f5cf
JoaoPedroCanavez <jsantossilvano@gmail.com>
2025-11-04 06:41:43 -0300

    (Add) Conexão com mongoDB e testes para verificar integridade da conexão.

--- testes.py
+++ testes.py
@@ -1,4 +1,5 @@
 from clients.redis_client import RedisClient
+from clients.mongo_client import MongoDBClient
 from utils.logger import logger
 
 data: dict = {
@@ -7,10 +8,15 @@ data: dict = {
     "age":21
 }
 
+''' Testes de query redis
 r = RedisClient()
-
-#r.create_queue(data=data)
+r.create_queue(data=data)
 r.del_queue("555")
 to_print = r.read_queue("555")
-logger.info(to_print)
+'''
+m = MongoDBClient()
 
+#m.add_query("555", data)
+logger.info(f"[Teste]{m.select_query("555")}")
+m.delete_query("555")
+logger.info(f"[Teste]{m.select_query("555")}")
